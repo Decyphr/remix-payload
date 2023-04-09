61701c8436d34a1c92ab698aadd12d0a071cebd2
blakehartman <bhartman@clickrain.com>
2023-04-09 17:49:42 -0400

    Remix v2 updates

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -1,105 +1,115 @@
 import type { Page, User } from '@org/cms';
 import type {
-    ErrorBoundaryComponent,
-    LinksFunction,
-    LoaderFunction,
-    MetaFunction,
-    TypedResponse,
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+  TypedResponse,
 } from '@remix-run/node';
-import { redirect } from '@remix-run/node';
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
 } from '@remix-run/react';
 
 import uiStyles from '@org/ui/styles.css';
 import styles from './styles/global.css';
-import { V2_ErrorBoundaryComponent } from '@remix-run/react/dist/routeModules';
 
 export const meta: MetaFunction = () => ({
-    charset: 'utf-8',
-    title: 'Payload CMS & Remix Monorepo',
-    viewport: 'width=device-width,initial-scale=1',
+  charset: 'utf-8',
+  title: 'Payload CMS & Remix Monorepo',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export const links: LinksFunction = () => [
-    {
-        rel: 'stylesheet',
-        href: uiStyles,
-    },
-    {
-        rel: 'stylesheet',
-        href: styles,
-    },
+  {
+    rel: 'stylesheet',
+    href: uiStyles,
+  },
+  {
+    rel: 'stylesheet',
+    href: styles,
+  },
 ];
 
 export type RootLoaderData = {
-    pages: Page[];
-    user?: {
-        user?: User;
-        token?: string;
-        exp?: number;
-    };
+  pages: Page[];
+  user?: {
+    user?: User;
+    token?: string;
+    exp?: number;
+  };
 };
 export const loader: LoaderFunction = async ({
-    context: { payload, user },
-    request,
+  context: { payload, user },
+  request,
 }): Promise<RootLoaderData | TypedResponse<never>> => {
+  const { docs: pages } = await payload.find({
+    collection: 'pages',
+    user,
+    overrideAccess: false,
+  });
 
-    const { docs: pages } = await payload.find({
-        collection: 'pages',
-        user,
-        overrideAccess: false,
-    });
-
-    return { pages, user };
+  return { pages, user };
 };
 
 export default function App() {
-    return (
-        <html lang="en">
-            <head>
-                <Meta />
-                <Links />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin=""
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
-                    rel="stylesheet"
-                />
-            </head>
-            <body>
-                <Outlet />
-                <ScrollRestoration />
-                <Scripts />
-                <LiveReload />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
 
-export const ErrorBoundary: V2_ErrorBoundaryComponent = () => {
+import { isRouteErrorResponse, useRouteError } from '@remix-run/react';
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  // when true, this is what used to go to `CatchBoundary`
+  if (isRouteErrorResponse(error)) {
     return (
-        <html lang="en">
-            <head>
-                <Meta />
-                <Links />
-            </head>
-            <body>
-                <div>ERROR:</div>
-                <Outlet />
-                <ScrollRestoration />
-                <Scripts />
-                <LiveReload />
-            </body>
-        </html>
+      <div>
+        <h1>Oops</h1>
+        <p>Status: {error.status}</p>
+        <p>{error.data.message}</p>
+      </div>
     );
-};
+  }
+
+  // TODO: Don't forget to typecheck with your own logic.
+  // Any value can be thrown, not just errors!
+  let errorMessage = 'Unknown error';
+  /* if (isDefinitelyAnError(error)) {
+      errorMessage = error.message;
+    } */
+
+  return (
+    <div>
+      <h1>Uh oh ...</h1>
+      <p>Something went wrong.</p>
+      <pre>{errorMessage}</pre>
+    </div>
+  );
+}
--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -1,7 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
     future: {
+        v2_errorBoundary: true,
+        v2_meta: true,
+        v2_normalizeFormMethod: true,
         v2_routeConvention: true,
+        unstable_tailwind: true
     },
     ignoredRouteFiles: ['**/.*'],
     assetsBuildDirectory: 'public/web/build',
